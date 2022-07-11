02d1982d34a3d49f6e51816e6a0836fdfc0ac1ce
Kurt Kanzenbach <kurt@linutronix.de>
2022-07-11 15:28:32 +0200

    configuration: Add application parameters for configuration
    Add application parameters for configuration.
    
    Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>

--- src/modules/configuration/module_configuration.c
+++ src/modules/configuration/module_configuration.c
@@ -16,6 +16,7 @@
 #include "../../common.h"
 #include "../../events_definitions.h"
 #include "../../helper/json_serializer.h"
+
 #include "module_configuration.h"
 
 //---------------------------------------
@@ -48,19 +49,158 @@ static void configuration_log(const char * restrict func, const char * restrict 
     va_end(args);
 }
 
+static void configuration_fill_app_param(struct configuration_parameter *parameter,
+                                         const TSN_App *app)
+{
+    int i;
+
+    for (i = 0; i < app->count_parameters; ++i) {
+        TSN_App_Parameter *par = &app->parameters[i];
+
+        /* OPC/UA Configuation URI */
+        if (!strcmp(par->name, "opcua_configuration_uri"))
+            parameter->opcua_configuration_uri = parameter_data_value_to_string(par);
+
+        /* vPLC parameter */
+        if (!strcmp(par->name, "vplc_current_status"))
+            parameter->vplc.current_status = parameter_data_value_to_string(par);
+
+        if (!strcmp(par->name, "vplc_commanded_status"))
+            parameter->vplc.commanded_status = parameter_data_value_to_string(par);
+
+        if (!strcmp(par->name, "vplc_axes")) {
+            char *axes;
+
+            axes = parameter_data_value_to_string(par);
+            parameter->vplc.axes = atoi(axes);
+            if (parameter->vplc.axes < 0)
+                parameter->vplc.axes = 0;
+            free(axes);
+        }
+
+        if (!strcmp(par->name, "vplc_speed")) {
+            char *speed;
+
+            speed = parameter_data_value_to_string(par);
+            parameter->vplc.speed = atof(speed);
+            free(speed);
+        }
+
+        if (!strcmp(par->name, "vplc_length")) {
+            char *length;
+
+            length = parameter_data_value_to_string(par);
+            parameter->vplc.length = atof(length);
+            free(length);
+        }
+
+        /* Execution parameter */
+        if (!strcmp(par->name, "exec_cycle_time")) {
+            char *time, *endptr;
+
+            time = parameter_data_value_to_string(par);
+            parameter->exec.cycle_time = strtoull(time, &endptr, 10);
+            if (errno != 0 || endptr == time || *endptr != '\0')
+                parameter->exec.cycle_time = CONFIGURATION_DEFAULT_CYCLE_TIME_NS;
+            free(time);
+        }
+
+        if (!strcmp(par->name, "exec_base_time")) {
+            char *time, *endptr;
+
+            time = parameter_data_value_to_string(par);
+            parameter->exec.base_time = strtoull(time, &endptr, 10);
+            if (errno != 0 || endptr == time || *endptr != '\0')
+                parameter->exec.base_time = CONFIGURATION_DEFAULT_BASE_TIME_NS;
+            free(time);
+        }
+
+        if (!strcmp(par->name, "exec_wakeup_latency")) {
+            char *time, *endptr;
+
+            time = parameter_data_value_to_string(par);
+            parameter->exec.wakeup_latency = strtoull(time, &endptr, 10);
+            if (errno != 0 || endptr == time || *endptr != '\0')
+                parameter->exec.wakeup_latency = CONFIGURATION_DEFAULT_WAKEUP_LATENCY_NS;
+            free(time);
+        }
+
+        if (!strcmp(par->name, "exec_scheduling_priority")) {
+            char *prio;
+
+            prio = parameter_data_value_to_string(par);
+            parameter->exec.scheduling_priority = atoi(prio);
+            if (parameter->exec.scheduling_priority <= 0)
+                parameter->exec.scheduling_priority = CONFIGURATION_DEFAULT_SCHEDULING_PRIORITY;
+            free(prio);
+        }
+
+        if (!strcmp(par->name, "exec_socket_priority")) {
+            char *prio;
+
+            prio = parameter_data_value_to_string(par);
+            parameter->exec.socket_priority = atoi(prio);
+            if (parameter->exec.socket_priority < 0)
+                parameter->exec.scheduling_priority = CONFIGURATION_DEFAULT_SOCKET_PRIORITY;
+            free(prio);
+        }
+    }
+}
+
+static void configuration_free_app_param(struct configuration_parameter *parameter)
+{
+    if (!parameter)
+        return;
+
+    free((void *)parameter->opcua_configuration_uri);
+    free((void *)parameter->vplc.current_status);
+    free((void *)parameter->vplc.commanded_status);
+}
+
 // ------------------------------------
 // Callback handler
 // ------------------------------------
 static void _cb_event(TSN_Event_CB_Data data)
 {
+    struct configuration_parameter param = { };
     const char *event_name = NULL;
+    TSN_App *app = NULL;
+    int ret;
 
-    if (data.event_id & EVENT_CONFIGURATION_DEPLOY)
+    // Initial parameter deployment
+    if (data.event_id & EVENT_CONFIGURATION_DEPLOY) {
         event_name = "EVENT_CONFIGURATION_DEPLOY";
-    if (data.event_id & EVENT_CONFIGURATION_CHANGED)
+
+        // Get app from sysrepo
+        ret = sysrepo_get_application_app(data.msg, &app);
+        if (ret != EXIT_SUCCESS)
+            goto out;
+
+        // Enrich app configuration parameters from sysrepo data
+        configuration_fill_app_param(&param, app);
+
+        // FIXME: Configure parameters via OPC/UA!
+    }
+
+    // Update run time parameters
+    if (data.event_id & EVENT_CONFIGURATION_CHANGED) {
         event_name = "EVENT_CONFIGURATION_CHANGED";
 
+        // Get app from sysrepo
+        ret = sysrepo_get_application_app(data.msg, &app);
+        if (ret != EXIT_SUCCESS)
+            goto out;
+
+        // Enrich app configuration parameters from sysrepo data
+        configuration_fill_app_param(&param, app);
+
+        // FIXME: Configure parameters via OPC/UA!
+    }
+
+out:
     log("Event '%s' (%s, %s)", event_name, data.entry_id, data.msg);
+    configuration_free_app_param(&param);
+    application_app_put(app);
 }
 
 // ------------------------------------
--- src/modules/configuration/module_configuration.h
+++ src/modules/configuration/module_configuration.h
@@ -6,6 +6,35 @@
 #ifndef _MODULE_CONFIGURATION_H_
 #define _MODULE_CONFIGURATION_H_
 
+#include <stdint.h>
 
+/* FIXME: Adapt to correct vPLC information model */
+struct vplc_parameter {
+    int32_t axes;
+    double speed;
+    double length;
+    const char *current_status;
+    const char *commanded_status;
+};
+
+struct execution_parameter {
+    uint64_t cycle_time;
+    uint64_t base_time;
+    uint64_t wakeup_latency;
+    int32_t scheduling_priority;
+    int32_t socket_priority;
+};
+
+struct configuration_parameter {
+    const char *opcua_configuration_uri;
+    struct execution_parameter exec;
+    struct vplc_parameter vplc;
+};
+
+static const uint64_t CONFIGURATION_DEFAULT_CYCLE_TIME_NS       = 1000000;
+static const uint64_t CONFIGURATION_DEFAULT_BASE_TIME_NS        = 0;
+static const uint64_t CONFIGURATION_DEFAULT_WAKEUP_LATENCY_NS   = 500000;
+static const int      CONFIGURATION_DEFAULT_SCHEDULING_PRIORITY = 1;
+static const int      CONFIGURATION_DEFAULT_SOCKET_PRIORITY     = 0;
 
 #endif /* _MODULE_CONFIGURATION_H_ */
